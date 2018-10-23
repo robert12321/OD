ed4c0f620f00568a5c4a647ca527b27e7c9648ad
robert12321 <roberto12321@tlen.pl>
2018-10-23 21:28:43 +0200

    commit

--- pcl_from_bag.py
+++ pcl_from_bag.py
@@ -6,21 +6,26 @@ import string
 import os #for file management make directory
 import shutil #for file management, copy file
 
-listOfBagFiles = ["2.bag"]
-listOfBagFiles = [f for f in os.listdir(".") if f[-4:] == ".bag"]
-bag = rosbag.Bag(listOfBagFiles[0])
-for subtopic, msg, t in bag.read_messages("/velodyne_points"):
-    array = numpy.empty([1000000,4])
-    i=0
-    print "Reading points to array"
-    for p in pc2.read_points(msg, field_names = ("x", "y", "z", "intensity"), skip_nans=True):
-  	print "%f %f %f %f" %(p[0],p[1],p[2],p[3])
-    	array[i][0] = p[0]
-	array[i][1] = p[1]
-	array[i][2] = p[2]
-	array[i][3] = p[3]
-	i = i+1
-    break;
-    #print "Saving array to binary file"
-    #array[:i][:].astype('float32').tofile("./PCL/"+str(t)+".bin")
-    
+listOfBagFiles = [".bag"]
+listOfBagFiles = [f for f in os.listdir(".") if f[-4:] == ".bag" and f[:3] == "pc-"]
+for BagFile in listOfBagFiles:
+    bag = rosbag.Bag(BagFile)
+    print "Proceseing "+BagFile
+    dirname = BagFile[:-4]
+    command = "mkdir "+dirname
+    os.system(command)
+    for subtopic, msg, t in bag.read_messages("/velodyne_points"):
+    	array = numpy.empty([1000000,4])
+    	i=0
+    	#print "Reading points to array"
+    	for p in pc2.read_points(msg, field_names = ("x", "y", "z", "intensity"), skip_nans=True):
+      	    #print "%f %f %f %f" %(p[0],p[1],p[2],p[3])
+    	    array[i][0] = p[0]
+	    array[i][1] = p[1]
+	    array[i][2] = p[2]
+	    array[i][3] = p[3]
+	    #i = i+1
+       	#print i
+    	#print "Saving array to binary file"
+    	array[:i][:].astype('float32').tofile(dirname+"/"+str(t)+".bin")
+
